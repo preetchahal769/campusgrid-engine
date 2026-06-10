845cf88494fe47f4b50430858a87739fc47cb261
Preet Chahal <cyberrayon@gmail.com>
2026-06-10 12:43:26 +0530

    fix(prisma): remove failing migration folder to allow container to start

--- prisma/migrations/20260609000000_change_status_enum/migration.sql
+++
@@ -1,4 +0,0 @@
-CREATE TYPE "BugReportStatus" AS ENUM ('OPEN', 'WORKING', 'SOLVED', 'REOPENED', 'CLOSED');
-ALTER TABLE "BugReport" ALTER COLUMN "status" DROP DEFAULT;
-ALTER TABLE "BugReport" ALTER COLUMN "status" TYPE "BugReportStatus" USING "status"::text::"BugReportStatus";
-ALTER TABLE "BugReport" ALTER COLUMN "status" SET DEFAULT 'OPEN';
